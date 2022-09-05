a46387cb8d46066c4c7ea6db3beb663c4066cf23
cheropChirchir7963 <cheropmarlene@gmail.com>
2022-09-05 23:37:50 +0300

    create users

---
+++ lib/tool_box/user.ex
@@ -0,0 +1,20 @@
+defmodule ToolBox.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :hashed_password, :string
+    field :session_secret, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :hashed_password, :session_secret])
+    |> validate_required([:email, :hashed_password, :session_secret])
+    |> unique_constraint(:email)
+  end
+end
