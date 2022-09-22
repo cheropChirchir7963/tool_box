2cebd02188587f9f3342b89397526cb1a2945140
amos-kibet <kibetamos97@gmail.com>
2022-09-22 10:44:13 +0300

    fix: address dependabot's secrets issue

--- sh/env.test.sh
+++ sh/env.test.sh
@@ -2,15 +2,16 @@
 
 export PG_DB_USER=postgres
 export PG_DB_PASSWORD=postgres
-export PG_DB_NAME=tool_box-test
+export PG_DB_NAME=tool_box_test
 export PG_DB_HOST=localhost
 export PG_DB_POOL_SIZE=10
 export PG_DB_URL=
-
-export PHX_HOST=http://localhost
-export BASE_URL=http://localhost
-export SECRET_KEY_BASE=XnGj8sRFQ6xWNa9fl2qe6l3GkgVxla5ht7dPsMXtVsudaTZWYGvTPPFvJE9P+Pxd
-export SALT=lclg47SPcD6rgsu5l7RQgPeLqnWXXwaCzjCi+EdauFdO1L6/e3g1iWOAfGUVXZ04
 export PORT=4000
 
-export JWT_SECRET_KEY=7YwpnIngo/tT4ywAiXNleeg+d91AlsYVGGETOLU/E0CUizmubm5ZYsYUDhHFci93
+
+# export PHX_HOST=http://localhost
+# export BASE_URL=http://localhost
+# export SECRET_KEY_BASE=XnGj8sRFQ6xWNa9fl2qe6l3GkgVxla5ht7dPsMXtVsudaTZWYGvTPPFvJE9P+Pxd
+# export SALT=lclg47SPcD6rgsu5l7RQgPeLqnWXXwaCzjCi+EdauFdO1L6/e3g1iWOAfGUVXZ04
+
+# export JWT_SECRET_KEY=7YwpnIngo/tT4ywAiXNleeg+d91AlsYVGGETOLU/E0CUizmubm5ZYsYUDhHFci93
