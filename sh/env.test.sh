17601767daf12910c24c4e5b8eb5da70c7d2657d
amos-kibet <kibetamos97@gmail.com>
2022-09-28 17:02:51 +0300

    fix: remove secrets

--- sh/env.test.sh
+++ sh/env.test.sh
@@ -7,11 +7,6 @@ export PG_DB_HOST=localhost
 export PG_DB_POOL_SIZE=10
 export PG_DB_URL=
 export PORT=4000
+export PHX_HOST=http://localhost
+export BASE_URL=http://localhost
 
-
-# export PHX_HOST=http://localhost
-# export BASE_URL=http://localhost
-# export SECRET_KEY_BASE=XnGj8sRFQ6xWNa9fl2qe6l3GkgVxla5ht7dPsMXtVsudaTZWYGvTPPFvJE9P+Pxd
-# export SALT=lclg47SPcD6rgsu5l7RQgPeLqnWXXwaCzjCi+EdauFdO1L6/e3g1iWOAfGUVXZ04
-
-# export JWT_SECRET_KEY=7YwpnIngo/tT4ywAiXNleeg+d91AlsYVGGETOLU/E0CUizmubm5ZYsYUDhHFci93
