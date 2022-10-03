84eeba11e977eae3c56f4bdfc15ccb87eb3acfcf
amos-kibet <kibetamos97@gmail.com>
2022-10-03 14:08:12 +0300

    fix: rename validate url changeset

--- lib/tool_box/box/box_item.ex
+++ lib/tool_box/box/box_item.ex
@@ -15,13 +15,13 @@ defmodule ToolBox.Box.BoxItem do
     box_item
     |> cast(attrs, [:url, :short_description, :snapshot])
     |> validate_required([:url, :short_description, :snapshot])
-    |> validate_url()
+    |> validate_url_changeset()
   end
 
   @doc """
   Validates that the url start with either http or https
   """
-  def validate_url(changeset) do
+  def validate_url_changeset(changeset) do
     regex = ~r/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
 
     changeset
