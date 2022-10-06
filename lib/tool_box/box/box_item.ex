8c28f87d529d61b502076d91dd3d68470874e50c
amos-kibet <kibetamos97@gmail.com>
2022-10-06 10:36:45 +0300

    fix: rename image validation changeset function

--- lib/tool_box/box/box_item.ex
+++ lib/tool_box/box/box_item.ex
@@ -21,11 +21,7 @@ defmodule ToolBox.Box.BoxItem do
   @doc """
   Validates that the url start with either http or https
   """
-<<<<<<< HEAD
   def validate_url_changeset(changeset) do
-=======
-  def validate_url(changeset) do
->>>>>>> 99eae900556666c42c69f0015e4186ae475c0340
     regex =
       ~r/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
 
